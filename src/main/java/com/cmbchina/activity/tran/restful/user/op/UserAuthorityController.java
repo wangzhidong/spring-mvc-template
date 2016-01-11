76912b31b091116c443b6b1dd9ea4afe60184719
wangtingbang <wangtingbang@wangtingbangdeMacBook-Pro.local>
2016-01-11 15:12:00 +0800

    commit

--- src/main/java/com/cmbchina/activity/tran/restful/user/op/UserAuthorityController.java
+++ src/main/java/com/cmbchina/activity/tran/restful/user/op/UserAuthorityController.java
@@ -1,7 +1,22 @@
 package com.cmbchina.activity.tran.restful.user.op;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created by wangtingbang on 15/12/10.
  */
+
+@Controller
+@RequestMapping(value="user/op")
 public class UserAuthorityController {
+  @RequestMapping(value ="{key}/login", method= RequestMethod.POST)
+  public ModelAndView userLogin( @PathVariable("key") String key,
+    String userName, String password ){
+    ModelAndView mav = new ModelAndView();
+    return mav;
+  }
 }
