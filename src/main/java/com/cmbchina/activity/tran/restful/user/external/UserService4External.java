4a57d541fa93a4d6d25b42740d9a758ab7fcc353
wangtingbang <wangtingbang@cmbchina.com>
2016-02-16 18:21:32 +0800

    user service for qua

--- src/main/java/com/cmbchina/activity/tran/restful/user/external/UserService4External.java
+++ src/main/java/com/cmbchina/activity/tran/restful/user/external/UserService4External.java
@@ -1,12 +1,26 @@
 package com.cmbchina.activity.tran.restful.user.external;
 
+import com.cmbchina.activity.busi.common.dto.AuthUser;
+import com.cmbchina.activity.busi.common.dto.ComBusiContext;
+import com.cmbchina.activity.busi.common.dto.ComUser;
+import com.cmbchina.activity.busi.common.service.AuthorityService;
+import com.cmbchina.activity.busi.common.service.ComUserService;
 import com.cmbchina.commons.bean.BusinessException;
+import com.cmbchina.commons.bean.CommonContext;
+import com.cmbchina.commons.util.DateTimeUtils;
+import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by wangtingbang on 16/1/13.
@@ -17,6 +31,18 @@ public class UserService4External {
 
   private static final Logger log = LoggerFactory.getLogger(UserService4External.class);
 
+  private AuthorityService authorityService;
+
+  private ComUserService comUserService;
+
+  public void setAuthorityService(AuthorityService authorityService){
+    this.authorityService = authorityService;
+  }
+
+  public void setComUserService(ComUserService comUserService){
+    this.comUserService = comUserService;
+  }
+
   /**
    * 用户登录验证，主要面向资格平台
    * @param token
@@ -25,8 +51,67 @@ public class UserService4External {
    */
   @RequestMapping(value = "validUserToken", method = RequestMethod.POST)
   @ResponseBody
-  public Object validUserToken(Object token) throws BusinessException{
+  public Map validUserToken(String token,HttpServletRequest request) throws BusinessException{
 
-    return null;
+    AuthUser user = authorityService.getUserByToken(token);
+
+    if(user == null){
+      return null;
+    }
+
+    Map<String, String> result = new HashMap<String, String>();
+    result.put("userId", user.getUserId());
+    result.put("userName",user.getUserName());
+    result.put("deptId",user.getDeptId());
+    result.put("deptName",user.getDeptName());
+    result.put("roleId",user.getRoleId());
+
+    return result;
+  }
+
+  /**
+   * 部门人员列表，主要面向资格平台
+   * @param page
+   * @param limit
+   * @param deptId
+   * @param roleList
+   * @param request
+     * @return
+     */
+  @RequestMapping(value = "listUsersByDept", method = RequestMethod.POST)
+  @ResponseBody
+  public List<Map> listUsersByDept(int page, int limit, String deptId, Byte[] roleList, HttpServletRequest request){
+
+    ComBusiContext commonContext = new ComBusiContext();
+    commonContext.setRequestSeqNo("EXTR"+DateTimeUtils.now()); //TODO seqNo-gen
+
+    /**
+     * TODO
+     */
+    String remoteAddr = request.getRemoteAddr();
+    String remoteHost = request.getRemoteHost();
+    String remoteUser = request.getRemoteUser();
+    String requestedSessionId = request.getRequestedSessionId();
+    Integer remotePort = request.getRemotePort();
+
+    List<Byte> role_s = Lists.newArrayList(roleList);
+
+    List<ComUser> users = comUserService.listUserByDept(commonContext, deptId, role_s, page, limit);
+
+    if(users == null || users.size() == 0){
+      return null;
+    }
+
+    List<Map> result = new ArrayList<Map>();
+    for(ComUser user: users){
+      Map<String, Object> map = new HashMap<String, Object>();
+      map.put("userId", user.getUserId());
+      map.put("userName",user.getUserName());
+      map.put("roleId", user.getRoleId());
+
+      result.add(map);
+    }
+
+    return result;
   }
 }
