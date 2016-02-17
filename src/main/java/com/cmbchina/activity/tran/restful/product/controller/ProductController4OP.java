eb37543c6a956c0a8fcf14d23a248a8ed0576c2b
wangtingbang <wangtingbang@cmbchina.com>
2016-02-17 15:20:04 +0800

    proudct controller

--- src/main/java/com/cmbchina/activity/tran/restful/product/controller/ProductController4OP.java
+++ src/main/java/com/cmbchina/activity/tran/restful/product/controller/ProductController4OP.java
@@ -1,9 +1,21 @@
 package com.cmbchina.activity.tran.restful.product.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.cmbchina.activity.busi.common.dto.AuthUser;
+import com.cmbchina.activity.busi.common.service.AuthorityService;
+import com.cmbchina.activity.busi.external.dto.ExternalProduct;
+import com.cmbchina.activity.busi.external.service.ExternalProductService;
+import com.cmbchina.commons.bean.BusinessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by wangtingbang on 16-2-17.
@@ -13,4 +25,98 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "operation/product")
 public class ProductController4OP {
   private static final Logger log = LoggerFactory.getLogger(ProductController4OP.class);
+
+  @Reference
+  private ExternalProductService externalProductService;
+
+  @Reference
+  private AuthorityService authorityService;
+
+  public void setExternalProductService(ExternalProductService externalProductService){
+    this.externalProductService = externalProductService;
+  }
+
+  public void setAuthorityService(AuthorityService authorityService){
+    this.authorityService = authorityService;
+  }
+
+  private List queryProductListByUserId(String userId, int page, int limit){
+
+    AuthUser user = null;
+    try {
+      user = authorityService.getUserByUserId(userId);
+    }catch (BusinessException e){
+      log.error("error:{}", e);
+      return null;
+    }catch (Exception e){
+      log.error("error:{}", e);
+      return null;
+    }
+
+    if(user == null){
+      log.error("user not login:{}", userId);
+      return null;
+    }
+
+    List<ExternalProduct> products = externalProductService.listAvailableProduct(
+            user.getAreaCode(), user.getDeptId(), page,limit);
+
+    return products;
+  }
+
+  /**
+   *
+   * @param userId
+   * @param request
+   * @return
+     */
+  public List<Map> listProductsIndex(String userId, HttpServletRequest request){
+
+    List<ExternalProduct> products = this.queryProductListByUserId(userId, -1, -1);
+    if(products == null || products.size() == 0){
+      return null;
+    }
+
+    List<Map> result = new ArrayList<Map>();
+    for(ExternalProduct product:products){
+      Map map = new HashMap();
+      map.put("productId",product.getProductId());
+      map.put("productName",product.getProductName());
+      map.put("productType",product.getProductType());
+      result.add(map);
+    }
+    return result;
+  }
+
+
+  public List listProducts(String userId, HttpServletRequest request){
+
+    List<ExternalProduct> products = this.queryProductListByUserId(userId, -1, -1);
+
+    if(products == null){
+      return null;
+    }
+
+    List<Map> result = new ArrayList<Map>();
+    for(ExternalProduct product:products){
+      Map map = new HashMap();
+      map.put("productId", product.getProductId());
+      map.put("productName", product.getProductName());
+      map.put("productType", product.getProductType());
+      map.put("sellPrice", product.getSellPrice());
+      map.put("marketPrice", product.getMarketPrice());
+      map.put("stockBalance", product.getStockBalance());
+      map.put("commitUserId", product.getCommitUserId());
+      map.put("commitUserName", product.getCommitUserName());
+      map.put("commitTime", product.getCommitTime());
+      map.put("status", product.getStatus());
+      map.put("actGroupId", product.getActGroupId());
+      map.put("actGroupName", product.getActGroupName());
+      map.put("activityId", product.getActivityId());
+      map.put("activityName", product.getActivityName());
+
+      result.add(map);
+    }
+    return result;
+  }
 }
--- src/main/java/com/cmbchina/activity/tran/restful/qualification/controller/QualificationController4External.java
+++ src/main/java/com/cmbchina/activity/tran/restful/qualification/controller/QualificationController4External.java
@@ -1,9 +1,16 @@
 package com.cmbchina.activity.tran.restful.qualification.controller;
 
+import com.cmbchina.activity.busi.common.dto.ComBusiContext;
+import com.cmbchina.activity.busi.external.service.ExternalQualificationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * Created by wangtingbang on 16-2-17.
@@ -11,8 +18,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "external/qualification")
 public class QualificationController4External {
+
   private static final Logger log = LoggerFactory.getLogger(QualificationController4External.class);
 
+  private ExternalQualificationService externalQualificationService;
 
+  public void setExternalQualificationService(ExternalQualificationService externalQualificationService){
+    this.externalQualificationService = externalQualificationService;
+  }
+
+  @RequestMapping(value = "listAvailableQualifications", method = RequestMethod.POST)
+  @ResponseBody
+  public List listAvailableQuas(String areaCode, String deptId, String userId, int page, int limit, HttpServletRequest request){
+
+    List result = this.externalQualificationService.listAvailableQualifications(areaCode,deptId, page, limit);
+
+    return result;
+  }
+
+  @RequestMapping(value = "listSubQuas", method = RequestMethod.POST)
+  @ResponseBody
+  public List listSubQuas( String quaGroupId, HttpServletRequest request){
+
+    ComBusiContext context = new ComBusiContext(); //TODO
+
+    List result = this.externalQualificationService.getSubQualifications(quaGroupId);
+
+    return result;
+  }
 
 }
