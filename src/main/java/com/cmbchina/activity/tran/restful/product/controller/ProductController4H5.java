a3988f5af9396a8af3e20dde8c98c42240b52549
wangtingbang <wangtingbang@cmbchina.com>
2016-02-17 11:12:46 +0800

    update, structure

---
+++ src/main/java/com/cmbchina/activity/tran/restful/product/controller/ProductController4H5.java
@@ -0,0 +1,12 @@
+package com.cmbchina.activity.tran.restful.product.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by wangtingbang on 16-2-17.
+ */
+@Controller
+@RequestMapping(value = "h5/product")
+public class ProductController4H5 {
+}
---
+++ src/main/java/com/cmbchina/activity/tran/restful/qualification/vo/QualificationRequest.java
@@ -0,0 +1,7 @@
+package com.cmbchina.activity.tran.restful.qualification.vo;
+
+/**
+ * Created by wangtingbang on 16-2-17.
+ */
+public class QualificationRequest {
+}
