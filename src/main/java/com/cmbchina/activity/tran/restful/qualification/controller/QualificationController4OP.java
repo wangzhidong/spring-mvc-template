cdb744804452c01abb4e9a3100bf0be3d6fd1628
wangtingbang <wangtingbang@wangtingbangdeMacBook-Pro.local>
2016-02-29 17:38:47 +0800

    request mapping for QualificationController4OP

--- src/main/java/com/cmbchina/activity/tran/restful/qualification/controller/QualificationController4OP.java
+++ src/main/java/com/cmbchina/activity/tran/restful/qualification/controller/QualificationController4OP.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -21,6 +23,8 @@ public class QualificationController4OP {
   @Autowired
   ExternalQualificationService externalQualificationService;
 
+  @RequestMapping(value = "listAvailableQualifications", method = RequestMethod.GET)
+  @ResponseBody
   public List listAvailableQualifications(String areaCode, String deptId, int page, int limit){
 
     log.info("listAvailableQualifications req: areaCode={}, deptId={}, page={}, limit={}", areaCode, deptId, page, limit);
