cbe977c9ecc78b254ae8108918f63f7757af9d50
wangtingbang <wangtingbang@wangtingbangdeMacBook-Pro.local>
2016-03-10 14:40:00 +0800

    datetime formatter

---
+++ src/main/java/com/cmbchina/activity/tran/support/mvc/DateTimeFormatAnnotationFormatterFactory.java
@@ -0,0 +1,48 @@
+package com.cmbchina.activity.tran.support.mvc;
+
+/**
+ * Created by wangtingbang on 16/3/10.
+ */
+import java.lang.annotation.Annotation;
+
+import org.joda.time.format.DateTimeFormatter;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.datetime.joda.JodaDateTimeFormatAnnotationFormatterFactory;
+
+import com.cmbchina.commons.util.DateTimeUtils;
+
+public class DateTimeFormatAnnotationFormatterFactory
+    extends JodaDateTimeFormatAnnotationFormatterFactory {
+  @Override
+  protected DateTimeFormatter getFormatter(final DateTimeFormat annotation, Class<?> fieldType) {
+    if ("".equals(annotation.pattern()) || null == annotation.pattern()) {
+      DateTimeFormat a = new DateTimeFormat() {
+
+        @Override
+        public Class<? extends Annotation> annotationType() {
+          return annotation.annotationType();
+        }
+
+        @Override
+        public String style() {
+          // TODO Auto-generated method stub
+          return annotation.style();
+        }
+
+        @Override
+        public String pattern() {
+          return DateTimeUtils.Pattern.DEFAULT_FORMATE_TIME;
+        }
+
+        @Override
+        public ISO iso() {
+          // TODO Auto-generated method stub
+          return annotation.iso();
+        }
+      };
+      return super.getFormatter(a, fieldType);
+    }
+    return super.getFormatter(annotation, fieldType);
+  }
+}
+
