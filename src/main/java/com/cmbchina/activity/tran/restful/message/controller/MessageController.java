3370f549527d8c4de6b4b1c682400eb5fc7d8314
wangtingbang <wangtingbang@wangtingbangdeMacBook-Pro.local>
2016-03-15 19:06:01 +0800

    addmessage

--- src/main/java/com/cmbchina/activity/tran/restful/message/controller/MessageController.java
+++ src/main/java/com/cmbchina/activity/tran/restful/message/controller/MessageController.java
@@ -12,12 +12,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by wangtingbang on 16/1/13.
@@ -55,17 +59,31 @@ public class MessageController {
 
   @RequestMapping(value = "addMessage", method = RequestMethod.POST)
   @ResponseBody
-  public int addMessage(Object message, HttpServletRequest request) throws BusinessException{
+  public int addMessage(@RequestBody HashMap req, HttpServletRequest request) throws BusinessException{
 
     ComBusiContext context = new ComBusiContext(); //TODO
 
-    log.info(JSONObject.toJSONString(message));
+    log.info(JSONObject.toJSONString(req));
 
     int result = -1;
 
-    ComMessage comMessage = (ComMessage) message; //TODO
+    Map param = (Map)req.get("param");
+    String commitUserId = (String)param.get("commitUserId");
+    Byte messageType = Byte.parseByte((String) param.get("messageType"));
+    Date commitTime = DateTimeUtils.now();//(String)param.get("commitUserId");
+    Byte channel = Byte.parseByte((String) param.get("channel"));
+    String description = (String)param.get("description");
 
-    result = comMessageService.addMessage(context, comMessage);
+    ComMessage message = new ComMessage(); //TODO
+    message.setUserId(commitUserId); //TODO
+    message.setMessageType(messageType);
+    message.setCommitUserId(commitUserId);
+    message.setCommitUserName(null);//TODO
+    message.setChannel(channel);
+    message.setCommitTime(commitTime);
+    message.setDescription(description);
+
+    result = comMessageService.addMessage(context, message);
     return result;
   }
 }
